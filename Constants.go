bb0e9cceec9c63ba355c9f30ca41c126db9b59b9
PRERIT KHANDELWAL <30365876+preritk@users.noreply.github.com>
2018-05-08 23:30:16 +0530

    comments updated

--- Constants.go
+++ Constants.go
@@ -3,10 +3,10 @@
 //Can come at places where a variable can come
 package main
 import "fmt"
-const a string = "David"  //Global variable
+const a string = "David"  //Global constants
 func main(){
    const b = 2
-   fmt.Println(float32(b)+2)
+   fmt.Println(float32(b)+2)    //Local constants
    const c = 2.5
    fmt.Println(c)
 }
