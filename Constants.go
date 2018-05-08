02f972a405d59f27efdf88fc51403d8752c1b211
PRERIT KHANDELWAL <30365876+preritk@users.noreply.github.com>
2018-05-08 23:29:32 +0530

    Source code added

---
+++ Constants.go
@@ -0,0 +1,12 @@
+//Constants in Go
+//Declared by const keyword.
+//Can come at places where a variable can come
+package main
+import "fmt"
+const a string = "David"  //Global variable
+func main(){
+   const b = 2
+   fmt.Println(float32(b)+2)
+   const c = 2.5
+   fmt.Println(c)
+}
